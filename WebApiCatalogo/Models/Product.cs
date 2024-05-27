8b69f749f6e9e722c2367dfb99ac5c2d645419a0
rafael isac <rafael.darrigo@gmail.com>
2024-05-27 13:03:48 -0300

    Configurando o projeto para usar o Entity Framework

--- WebApiCatalogo/Models/Product.cs
+++ WebApiCatalogo/Models/Product.cs
@@ -1,6 +1,17 @@
-﻿namespace WebApiCatalogo.Models
+﻿namespace WebApiCatalogo.Models;
+
+/*este exemplo não tem imagens armazenadas no banco , apenas o caminho para exemplificar
+ * não esquecer de instalar o Pomelo.EntityFrameworkCore.Mysql e o 
+ * Microsoft.EntityFrameworkCore.Design para fazer a lincagem com o banco e o 
+ * Microsoft.EntityFrameworkCore.Tools para usar o migrations
+ 
+ */
+public class Product
 {
-    public class Product
-    {
-    }
+    public int ProductId { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public  string? ImageUrl { get; set; }
+    public float stock { get; set; }
 }
