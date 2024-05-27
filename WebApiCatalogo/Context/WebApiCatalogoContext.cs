3be188eea92b655e1a008387e70ed26b28e9168c
rafael isac <rafael.darrigo@gmail.com>
2024-05-27 14:51:32 -0300

    Criando a classe de contexto do Entity Framework Core

---
+++ WebApiCatalogo/Context/WebApiCatalogoContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiCatalogo.Models;
+
+namespace WebApiCatalogo.Context
+
+    /*Essa classe serve para especificar no banco de dados nossas Entidades/models,mostrar o que é  tabela,
+     * o que e campo da tabela
+     * Só posso usar se tiver o Microsoft.EntityFrameworkCore
+     * "Usado para realizar a comunicação entre as minhas entidades e o banco de dados "
+     * mapeamento das entidades
+     */
+{
+    public class WebApiCatalogoContext : DbContext
+    {
+        public WebApiCatalogoContext(DbContextOptions<WebApiCatalogoContext> options) : base(options) //no construtor voce define um parametro
+        {
+            
+        }
+        /*use como base a entidade que deseja usar como exemplo da tabela e depois o nome em plural */
+        public DbSet<Category>? Categories { get; set; } 
+        public DbSet<Product>? Products { get; set; } 
+    }
+}
