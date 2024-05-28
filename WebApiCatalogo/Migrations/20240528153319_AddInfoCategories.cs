3f99521116f6eb49ca485e7821be0ee33ef426b5
rafael isac <rafael.darrigo@gmail.com>
2024-05-28 12:47:07 -0300

    Adcionando informações a tabela Categories do banco de dados

---
+++ WebApiCatalogo/Migrations/20240528153319_AddInfoCategories.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+//adcionando valores a tabela
+
+#nullable disable
+
+namespace WebApiCatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInfoCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Categories(Name,ImageUrl) values('Bebidas','bebidas.jpg')");
+            mb.Sql("Insert into Categories(Name,ImageUrl) values('Lanches','lanches.jpg')");
+            mb.Sql("Insert into Categories(Name,ImageUrl) values('Sobremesas','sobremesas.jpg')");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+
+            mb.Sql("Delete from Categories");
+
+        }
+    }
+}
